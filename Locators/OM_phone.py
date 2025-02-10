d12917345340cd0dd8e52cbe2aef018404eb2187
Vinay <vinaybakoria99@gmail.com>
2025-02-10 22:36:59 +0530

    This is my fifth commit

--- Locators/OM_phone.py
+++ Locators/OM_phone.py
@@ -1,18 +1,41 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium import webdriver
 import time
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoAlertPresentException
+
 class Om_phone:
     lnk_OM_phone_Xpath = "//p[normalize-space()='+91 81064 41105']"
-    def __init__(self,driver):
+
+    def __init__(self, driver):
         self.driver = driver
+
     def click_om_phonelink(self):
-        self.act = ActionChains(self.driver)
-        self.driver.find_element(By.XPATH, self.lnk_OM_phone_Xpath).click()
-        self.myalert = self.driver.switch_to.alert
-        self.myalert.accept()
-        time.sleep(10)
+        try:
+            # Click the phone link
+            self.driver.find_element(By.XPATH, self.lnk_OM_phone_Xpath).click()
+
+            # Wait for the alert to be present
+            WebDriverWait(self.driver, 10).until(EC.alert_is_present())
+
+            # Switch to the alert
+            alert = self.driver.switch_to.alert
+
+            # Accept the alert
+            alert.accept()
+
+            # Optional: Wait for a few seconds after accepting the alert
+            time.sleep(2)
+
+        except TimeoutException:
+            print("Timed out waiting for alert to appear.")
+        except NoAlertPresentException:
+            print("No alert present after clicking the phone link.")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
 
         
     
--- Testcases/test_om_phone.py
+++ Testcases/test_om_phone.py
@@ -4,11 +4,11 @@ from Utilities.customlogger import customlogger
 
 @pytest.mark.usefixtures("setup")
 class Test_OM_Phone:
-    def test_om_phone(self, driver):
+    def test_om_phone(self):
         self.logger = customlogger()
         self.log =self.logger.get_logger()
         self.log.info("*********Test OM Phone started***********")
-        self.op = Om_phone()
+        self.op = Om_phone(self.driver)
         self.op.click_om_phonelink()
         
         
