235e2b88b6c7c1f2692cea1deb000cd180c89b42
Vinay <vinaybakoria99@gmail.com>
2025-02-12 00:21:46 +0530

    This is my 7 commit

--- Testcases/test_searchbar.py
+++ Testcases/test_searchbar.py
@@ -3,7 +3,7 @@ from Utilities.customlogger import customlogger
 from Locators.Search_bar import Search_bar
 @pytest.mark.usefixtures("setup")
 class Test_searchbar:
-    def test_search_bar(self, driver):
+    def test_search_bar(self.driver):
         self.logger = customlogger()
         self.log = self.logger.get_logger()
         self.log.info("***********Test search_bart_001 started*************")
