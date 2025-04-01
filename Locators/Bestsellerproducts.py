3f7744736e6542b4011bf29c6837bda07025ee9a
Vinay <vinaybakoria99@gmail.com>
2025-04-01 17:32:29 +0530

    latest commit1

--- Locators/Bestsellerproducts.py
+++ Locators/Bestsellerproducts.py
@@ -1,9 +1,10 @@
 from selenium.webdriver.common.by import By
+import time
 
 class BestSEllerPRoduct:
     def __init__(self,driver):
         self.driver = driver
-    Addtocart_btn_Xpath = "/html/body/app-root/app-home-page/div[2]/app-product-slider/div/div/div/div[2]/ngx-slick-carousel/div/div/div[1]/app-product-card/div/div/div/div[2]/div/div/div/div/button//div[1]//div[1]//div[2]//div[1]//div[1]//div[1]//div[1]//button[1]"
+    Addtocart_btn_Xpath = "/html/body/app-root/app-home-page/div[2]/app-product-slider/div/div/div/div[2]/ngx-slick-carousel/div/div/div[1]/app-product-card/div/div/div/div[2]/div/div/div/div/button"
     Addproductquantity_Xpath = "//div[@class='quantitt-box ng-star-inserted']//span[@class='pi pi-plus']"
     item_cart_btn = "//div[@class='quantitt-box ng-star-inserted']//span[@class='pi pi-plus']"
     cat_btn_xpath = "//span[normalize-space()='Proceed']"
@@ -12,5 +13,6 @@ class BestSEllerPRoduct:
     Checkout_btn_xpath = "//span[normalize-space()='Checkout']"
     def add_tocart(self):
         self.driver.find_element(By.XPATH,self.Addtocart_btn_Xpath).click()
+        time.sleep(8)
     def add_product_quantity(self):
         self.driver.find_element(By.XPATH,self.Addproductquantity_Xpath).click()
